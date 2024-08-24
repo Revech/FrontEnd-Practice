237d4148049df4fc5b5da5099890805b5e7176b4
Reve Chahine <146188277+Revech@users.noreply.github.com>
2024-08-24 05:13:38 +0300

    Add files via upload

---
+++ JavaScript/ToDoList/script.js
@@ -0,0 +1,73 @@
+const todoList = [
+{
+    name:'',
+    dueDate :''
+}
+
+];
+
+render();
+function render()
+{
+    let todoHtml = '';
+
+    /*  Using ForEach as an alternative 
+    todoList.forEach(function( todoObject, index)
+{
+    const todoObject = todoList[i];
+    const name = todoObject.name;
+    const dueDate = todoObject.dueDate;
+    // shortcut cost{name,dueDate} = todoObject;
+    const html = `
+    <div>${name} </div>
+     <div>${dueDate} </div> 
+    <button class ="dbutton" onclick="
+    todoList.splice(${index},1);
+    render();
+    ">Delete</button> `;
+    todoHtml += html;
+  
+    
+
+document.querySelector('.div').innerHTML = todoHtml;
+}
+
+);*/
+
+    for (let i = 0; i < todoList.length; i ++)
+        {
+            const todoObject = todoList[i];
+            const name = todoObject.name;
+            const dueDate = todoObject.dueDate;
+            // shortcut cost{name,dueDate} = todoObject;
+            const html = `
+            <div>${name} </div>
+             <div>${dueDate} </div> 
+            <button class ="dbutton" onclick="
+            todoList.splice(${i},1);
+            render();
+            ">Delete</button> `;
+            todoHtml += html;
+          
+            
+        }
+        document.querySelector('.div').innerHTML = todoHtml;
+}
+
+ 
+
+
+function addTodo()
+{
+    const todo = document.querySelector('.input');
+    const todoDate = document.querySelector('.dateInput');
+
+    const dueDate = todoDate.value;
+    const name = todo.value;
+    todoList.push({name: name , dueDate : dueDate }); 
+    //shortcut  todoList.push({name , dueDate}); same name 
+
+    todo.value = '';
+    render();
+  
+}
